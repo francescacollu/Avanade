460f5bc930c829d5fbb431be2feae396cd86ce86
francescacollu <33123803+francescacollu@users.noreply.github.com>
2018-08-06 11:32:58 +0200

    AlbergoEntityFramework finale.

--- AlbergoEntityFramework/AlbergoEntityFramework/Program.cs
+++ AlbergoEntityFramework/AlbergoEntityFramework/Program.cs
@@ -11,17 +11,21 @@ namespace AlbergoEntityFramework
         static void Main(string[] args)
         {
 
-            EsempiEF.testLettura1();
-            EsempiEF.testInserimento();
-            EsempiEF.testLettura2();
-            EsempiEF.testAggiorno();
-            EsempiEF.testLettura1();
-            EsempiEF.testCancello();
-            EsempiEF.testLettura1();
-
+            UpdatePrenotazioni.LetturaPrenotazioni();
+            UpdatePrenotazioni.InserimentoPrenotazione();
+            UpdatePrenotazioni.LetturaPrenotazioni();
 
         }
 
-
     }
 }
+
+/*
+ * Al fine di preservare l'integrità dei dati, sarebbe utile implementare altri controlli di sicurezza:
+ * bisognerebbe controllare che le date di check-in siano precedenti alle date di check-out;
+ * bisognerebbe fare un controllo sulla esistenza nel DB del numero di camera inserita nella prenotazione,
+ * in un modo simile a quello fatto per il controllo sull'idCliente;
+ * bisognerebbe fare un controllo sulla disponibilità delle camere d'albergo nel periodo scelto dall'utente (per 
+ * farlo, si deve verificare la condizione di non sovrapposizione dei periodi scelti dall'utente con i periodi in 
+ * cui le stanze sono prenotate).
+*/
--- AlbergoEntityFramework/AlbergoEntityFramework/UpdatePrenotazioni.cs
+++ AlbergoEntityFramework/AlbergoEntityFramework/UpdatePrenotazioni.cs
@@ -10,25 +10,64 @@ namespace AlbergoEntityFramework
     {
         public static void InserimentoPrenotazione()
         {
-            Console.WriteLine("Inserisco cliente Marco Bianchetti");
+            Console.WriteLine("Inserisco una nuova prenotazione.");
             AlbergoEntities albergoDB;
             albergoDB = new AlbergoEntities();
             // inserisco una nuova prenotazione
             Prenotazioni prenotazione = new Prenotazioni();
-            prenotazione.periodoDal = "Via Milano 2";
-            prenotazione.periodoAl = "Marco Bianchetti";
-            prenotazione.camera = "444444444";
-            prenotazione.idCliente = "";
-            prenotazione.prezzoTotale = "";
 
-            try
+            Console.WriteLine("Inserire la data di check-in:");
+            System.DateTime checkIn = Convert.ToDateTime(Console.ReadLine());
+            prenotazione.periodoDal = checkIn;
+            Console.WriteLine("Inserire la data di check-out:");
+            System.DateTime checkOut = Convert.ToDateTime(Console.ReadLine());
+            prenotazione.periodoAl = checkOut;
+            Console.WriteLine("Inserire il numero di camera:");
+            short n_camera = Convert.ToInt16(Console.ReadLine());
+            prenotazione.camera = n_camera;
+            Console.WriteLine("Inserire l'id del cliente:");
+            int id_cliente = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserire il prezzo:");
+            decimal prezzo_ = Convert.ToDecimal(Console.ReadLine());
+            prenotazione.prezzoTotale = prezzo_;
+
+            if (albergoDB.Prenotazioni.Any(o => o.idCliente == id_cliente))
             {
-                albergoDB.Prenotazioni.Add(prenotazione);
-                albergoDB.SaveChanges();
+                prenotazione.idCliente = id_cliente;
+                try
+                {
+                    albergoDB.Prenotazioni.Add(prenotazione);
+                    albergoDB.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Messaggio di errore:{0}", ex.ToString());
+                }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine("Messaggio di errore:{0}", ex.ToString());
+                Console.WriteLine("IdCliente non valido.");
+            }
+            Console.ReadKey();
+        }
+
+        // Metodo di lettura delle prenotazioni
+        public static void LetturaPrenotazioni()
+        {
+
+            Console.WriteLine("Stampo tabella Prenotazioni");
+            AlbergoEntities albergoDB;
+            albergoDB = new AlbergoEntities();
+            //var: sarà il compilatore a definire implicitamente il tipo della variabile
+            var query1 = from p in albergoDB.Prenotazioni
+                         select p;
+
+            List<Prenotazioni> listaPrenotazioni = query1.ToList();
+
+            Console.WriteLine("\t{0} \t {1} \t\t {2} {3} {4}", "PeriodoDal", "PeriodoAl", "Camera", "IdCliente", "PrezzoTotale");
+            foreach (Prenotazioni pren in listaPrenotazioni)
+            {
+                Console.WriteLine("{0} \t {1} \t {2} \t {3} \t {4}", pren.periodoDal, pren.periodoAl, pren.camera, pren.idCliente, pren.prezzoTotale);
             }
             Console.ReadKey();
         }
